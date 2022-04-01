ff153649045e4cad3a1c3212361b15e9d4a60c78
Jens Walter <jens@apimeister.com>
2022-04-01 22:49:39 +0200

    add some error handling

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+FROM rust as builder
+RUN rustup component add rustfmt clippy
+WORKDIR /usr/lib/oracle
+RUN wget https://download.oracle.com/otn_software/linux/instantclient/instantclient-basiclite-linuxx64.zip
+RUN unzip -j instantclient-basiclite-linuxx64.zip
+WORKDIR /app
+ADD . .
+RUN cargo build --release
+
+FROM debian:stable-slim as runner
+ENV RUST_LOG=info
+ENV LD_LIBRARY_PATH=/usr/lib/oracle
+RUN apt-get update && apt-get install -y libaio1 && apt-get clean
+COPY --from=builder /usr/lib/oracle /usr/lib/oracle
+COPY --from=builder /app/target/release/keda-oracle-scaler /keda-oracle-scaler
+CMD ["/keda-oracle-scaler"]
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 
+use oracle::Connection;
 use service::external_scaler_server::ExternalScaler;
 use service::GetMetricSpecResponse;
 use service::GetMetricsRequest;
@@ -11,10 +12,21 @@ use service::external_scaler_server::ExternalScalerServer;
 use tokio_stream::wrappers::ReceiverStream;
 use tonic::{Request, Response, Status};
 use tonic::transport::Server;
+use env_var::env_var;
+use std::sync::Mutex;
+use once_cell::sync::Lazy;
 
 pub mod service {
     tonic::include_proto!("externalscaler");
 }
+
+static DB_CONNECTION: Lazy<Mutex<Connection>> = Lazy::new(|| {
+    let url = env_var!(required "DB_URL");
+    let username = env_var!(required "DB_USER");
+    let password = env_var!(required "DB_PASSWORD");
+    let conn = Connection::connect(username, password, url).unwrap();
+    Mutex::new(conn)
+});
 
 #[derive(Debug)]
 struct ScalerService;
@@ -25,37 +37,73 @@ impl ExternalScaler for ScalerService {
         &self,
         request: Request<GetMetricsRequest>,
     ) -> Result<Response<GetMetricsResponse>, Status> {
-        let params = request.metadata();
-        println!("{:?}", params);
-        Ok(Response::new(GetMetricsResponse {
-            metric_values: vec![MetricValue{
-                metric_name: "count".to_string(),
-                metric_value: 0,
-            }],
-        }))
+        let inner = request.into_inner();
+        let name = inner.metric_name;
+        let scaled_object = inner.scaled_object_ref.unwrap();
+        let metadata = scaled_object.scaler_metadata.clone();
+        log::debug!("{name}: {:?}", metadata);
+        if metadata.contains_key("query"){
+            let query = metadata.get("query").unwrap();
+            match DB_CONNECTION.lock() {
+                Ok(conn) => {
+                    let rows = conn.query(query, &[]).unwrap();
+                    let mut metric_value: i64 = 0;
+                    let mut counter = 0;
+                    for row_result in rows {
+                        let row = row_result.unwrap();
+                        counter += 1;
+                        if counter== 1 {
+                            let first_column: Result<i64, _> = row.get(0);
+                            match first_column {
+                                Ok(value) => metric_value = value,
+                                Err(_) => metric_value = counter,
+                            }
+                        }else{
+                            metric_value = counter;
+                        }
+                    }
+                    log::debug!("{metric_value} from {query}");
+                    return Ok(Response::new(GetMetricsResponse {
+                        metric_values: vec![MetricValue{
+                            metric_name: "count".to_string(),
+                            metric_value: metric_value,
+                        }],
+                    })); 
+                },
+                Err(_e) => {
+                    panic!("poison error, restarting...");
+                }
+            }
+        }else{
+            log::warn!("no query found in metadata: {:?}", scaled_object);
+            Ok(Response::new(GetMetricsResponse {
+                metric_values: vec![MetricValue{
+                    metric_name: "count".to_string(),
+                    metric_value: 0,
+                }],
+            }))
+        }
     }
     async fn is_active(
         &self,
-        request: Request<ScaledObjectRef>,
+        _request: Request<ScaledObjectRef>,
     ) -> Result<Response<IsActiveResponse>, Status> {
-        let params = request.metadata();
-        println!("{:?}", params);
         Ok(Response::new(IsActiveResponse {
             result: true,
         }))
     }
     async fn stream_is_active(
         &self,
-        request: Request<ScaledObjectRef>,
+        _request: Request<ScaledObjectRef>,
     ) -> Result<Response<Self::StreamIsActiveStream>, Status> {
-        let params = request.metadata();
-        println!("{:?}", params);
         Err(Status::unimplemented("stream_is_active is not implemented"))
     }
     async fn get_metric_spec(
         &self,
-        _request: Request<ScaledObjectRef>,
+        request: Request<ScaledObjectRef>,
     ) -> Result<Response<GetMetricSpecResponse>, Status> {
+        let inner = request.into_inner();
+        log::trace!("get_metric_spec: {:?}",inner);
         let result = GetMetricSpecResponse {
             metric_specs: vec![MetricSpec{
                 metric_name: "count".to_string(),
@@ -70,8 +118,11 @@ impl ExternalScaler for ScalerService {
 #[tokio::main]
 async fn main() {
     env_logger::init();
+    {
+        let _ = DB_CONNECTION.lock();
+    }
     log::info!("listening on port 10000");
-    let addr = "[::1]:10000".parse().unwrap();
+    let addr = "0.0.0.0:10000".parse().unwrap();
 
     let scaler_svc = ScalerService{};
 
