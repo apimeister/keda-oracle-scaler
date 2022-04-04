498bf316275cfd6937aa78fb83e11a5b47126530
Jens Walter <jens@apimeister.com>
2022-04-04 23:02:40 +0200

    fix isActive

--- src/main.rs
+++ src/main.rs
@@ -28,6 +28,34 @@ static DB_CONNECTION: Lazy<Mutex<Connection>> = Lazy::new(|| {
     Mutex::new(conn)
 });
 
+async fn run_query(query: &str) -> i64 {
+    match DB_CONNECTION.lock() {
+        Ok(conn) => {
+            let rows = conn.query(query, &[]).unwrap();
+            let mut metric_value: i64 = 0;
+            let mut counter = 0;
+            for row_result in rows {
+                let row = row_result.unwrap();
+                counter += 1;
+                if counter== 1 {
+                    let first_column: Result<i64, _> = row.get(0);
+                    match first_column {
+                        Ok(value) => metric_value = value,
+                        Err(_) => metric_value = counter,
+                    }
+                }else{
+                    metric_value = counter;
+                }
+            }
+            return metric_value;
+        },
+        Err(_e) => {
+            log::error!("poison error, restarting...");
+            std::process::exit(1);
+        }
+    }
+}
+
 #[derive(Debug)]
 struct ScalerService;
 
@@ -44,37 +72,14 @@ impl ExternalScaler for ScalerService {
         log::debug!("{name}: {:?}", metadata);
         if metadata.contains_key("query"){
             let query = metadata.get("query").unwrap();
-            match DB_CONNECTION.lock() {
-                Ok(conn) => {
-                    let rows = conn.query(query, &[]).unwrap();
-                    let mut metric_value: i64 = 0;
-                    let mut counter = 0;
-                    for row_result in rows {
-                        let row = row_result.unwrap();
-                        counter += 1;
-                        if counter== 1 {
-                            let first_column: Result<i64, _> = row.get(0);
-                            match first_column {
-                                Ok(value) => metric_value = value,
-                                Err(_) => metric_value = counter,
-                            }
-                        }else{
-                            metric_value = counter;
-                        }
-                    }
-                    log::debug!("{metric_value} from {query}");
-                    return Ok(Response::new(GetMetricsResponse {
-                        metric_values: vec![MetricValue{
-                            metric_name: "count".to_string(),
-                            metric_value: metric_value,
-                        }],
-                    })); 
-                },
-                Err(_e) => {
-                    log::error!("poison error, restarting...");
-                    std::process::exit(1);
-                }
-            }
+            let metric_value = run_query(query).await;
+            log::debug!("{metric_value} from query \"{query}\"");
+            return Ok(Response::new(GetMetricsResponse {
+                metric_values: vec![MetricValue{
+                    metric_name: "count".to_string(),
+                    metric_value: metric_value,
+                }],
+            })); 
         }else{
             log::warn!("no query found in metadata: {:?}", scaled_object);
             Ok(Response::new(GetMetricsResponse {
@@ -87,11 +92,27 @@ impl ExternalScaler for ScalerService {
     }
     async fn is_active(
         &self,
-        _request: Request<ScaledObjectRef>,
+        request: Request<ScaledObjectRef>,
     ) -> Result<Response<IsActiveResponse>, Status> {
-        Ok(Response::new(IsActiveResponse {
-            result: true,
-        }))
+        let inner = request.into_inner();
+        let metadata = inner.scaler_metadata.clone();
+        let mut metric_value:i64 = 0;
+        if metadata.contains_key("query"){
+            let query = metadata.get("query").unwrap();
+            metric_value = run_query(query).await;
+            log::debug!("{metric_value} from query \"{query}\"");
+        }else{
+            log::warn!("no query found in metadata: {:?}", metadata);
+        }
+        if metric_value > 0 {
+            Ok(Response::new(IsActiveResponse {
+                result: true,
+            }))    
+        }else{
+            Ok(Response::new(IsActiveResponse {
+                result: false,
+            }))    
+        }
     }
     async fn stream_is_active(
         &self,
